82d4e3316aab9140df729666eb7dfc08bfb5e009
“Andrew <andrewwoods88@gmail.com>
2017-12-29 14:22:06 -0500

    Add css file structure plus styles, set up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
 
   root to: 'pages#index'
 end

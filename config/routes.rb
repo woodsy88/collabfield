4553aa95b59bf10b59489be8a77244b3da847636
“Andrew <andrewwoods88@gmail.com>
2017-12-30 12:12:19 -0500

    Fixed bug with registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
-  end
-
-  devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
   end
 

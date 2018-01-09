6a6ae0fcea851574d61dd1907de8575563cb1145
“Andrew <andrewwoods88@gmail.com>
2018-01-09 14:53:11 -0500

    Add specs for User, Category, Post models' associations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
+# We use the described_class method to get the current context’s class.
+# Then we use reflect_on_association method to check that it returns a correct association.
+
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Associations' do
+    it 'belongs_to user' do
+      association = described_class.reflect_on_association(:user).macro
+      expect(association).to eq :belongs_to
+    end
+
+    it 'belongs_to category' do
+      association = described_class.reflect_on_association(:category).macro
+      expect(association).to eq :belongs_to
+    end
+  end
 end

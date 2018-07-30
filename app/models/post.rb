41287d9be9cf449d31d64f8598b80ad1e665aa53
“Andrew <andrewwoods88@gmail.com>
2018-07-30 16:39:34 -0400

    Define a default_scope for posts"

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  # orders posts in descending order by the creation date,
+  #  newest posts are at the top.
+  default_scope -> { includes(:user).order(created_at: :desc) }
 end
